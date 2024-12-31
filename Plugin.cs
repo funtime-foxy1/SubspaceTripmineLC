f01989fa68ed9cc0197b1d81528720db48f81dbc
FunFoxRR <dldilley01@gmail.com>
2024-12-30 23:06:02 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using LethalNetworkAPI;
+using UnityEngine;
+
+namespace SubspaceTripmineLC
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    [BepInDependency(MyPluginInfo.PLUGIN_GUID)]
+    public class STPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "funfoxrr.SubspaceTripmine";
+        public const string NAME = "Subspace Tripmine";
+        public const string VERSION = "0.0.0";
+
+        public static STPlugin Instance { get; private set; }
+
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        internal static ManualLogSource Log;
+        public static string assemblyLocation;
+        public static AssetBundle assets;
+
+        public static ConfigEntry<bool> enableExplosion;
+        public static ConfigEntry<bool> instantlyKillPlayer;
+        public static ConfigEntry<int> damageDone;
+        public static ConfigEntry<float> plantedTransparency;
+        public static ConfigEntry<bool> destroyAfterExplosion;
+
+        void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            
+            Log = BepInEx.Logging.Logger.CreateLogSource(GUID);
+            Log.LogInfo(GUID + " has loaded!");
+            assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            assets = AssetBundle.LoadFromFile(Path.Combine(assemblyLocation, "funfoxrr__subspacetripmine"));
+            if (assets == null)
+            {
+                Log.LogError("Failed to load custom assets.");
+                return;
+            }
+
+            enableExplosion = base.Config.Bind(
+                "Functionality",
+                "Enable Explosion Audio",
+                true,
+                "If the subspace plays the explosion sfx."
+            );
+            instantlyKillPlayer = base.Config.Bind(
+                "Functionality",
+                "Instantly kill player",
+                true,
+                "Kills the player on contact. Adjust damage with 'Damage' Setting."
+            );
+            damageDone = base.Config.Bind(
+                "Functionality",
+                "Damage",
+                90,
+                "The amount of damage done."
+            );
+            plantedTransparency = base.Config.Bind(
+                "Functionality",
+                "Planted Transparency",
+                .05f,
+                "How transparent the mine is when placed. Best used below 0.15"
+            );
+            destroyAfterExplosion = base.Config.Bind(
+                "Functionality",
+                "Destroy after explosion",
+                true,
+                "If the mine disappears when someone steps on it."
+            );
+
+
+            var rarityConfig = base.Config.Bind(
+                "Spawning",
+                "Spawn Rate",
+                50,
+                "The spawn rate. The higher, the more will spawn. Requires restart if using configuration mods"
+            );
+
+
+            var canBuyConfig = base.Config.Bind(
+                "Shop",
+                "Can Buy",
+                true,
+                "If it shows up in the shop. Requires restart if using configuration mods"
+            );
+            var priceConfig = base.Config.Bind(
+                "Shop",
+                "Price",
+                100,
+                "The price of the item. Requires restart if using configuration mods"
+            );
+
+            int rarity = rarityConfig.Value;
+            Item subspace = assets.LoadAsset<Item>("SubspaceTripmineItem.asset");
+            if (subspace == null)
+            {
+                Log.LogError("Failed to load SubspaceTripmineItem asset.");
+                return;
+            }
+            var prefab = subspace.spawnPrefab;
+            var comp = prefab.AddComponent<SubspaceTripmine>();
+            comp.grabbable = true;
+            comp.grabbableToEnemies = true;
+            comp.itemProperties = subspace;
+            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(subspace.spawnPrefab);
+            LethalLib.Modules.Items.RegisterScrap(subspace, rarity, LethalLib.Modules.Levels.LevelTypes.All);
+
+            if (canBuyConfig.Value)
+            {
+                int price = priceConfig.Value;
+                TerminalNode iTerminalNode = assets.LoadAsset<TerminalNode>("iTerminalNode.asset");
+                if (iTerminalNode == null)
+                {
+                    Log.LogError("Failed to load iTerminalNode asset.");
+                    return;
+                }
+                LethalLib.Modules.Items.RegisterShopItem(subspace, null, null, iTerminalNode, price);
+            }
+
+            //TODO: test
+            //harmony.PatchAll(typeof(RMPatch));
+        }
+    }
+}
---
+++ SubspaceTripmine.cs
@@ -0,0 +1,262 @@
+﻿using GameNetcodeStuff;
+using LethalLib.Modules;
+using LethalNetworkAPI;
+using LethalNetworkAPI.Serializable;
+using LethalNetworkAPI.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SubspaceTripmineLC
+{
+    class SubspaceTripmine : PhysicsProp
+    {
+        public LNetworkVariable<bool> isPlanted;
+        //public static LNetworkVariable<float> colora;
+
+        Coroutine fadeOut;
+
+        public LNetworkMessage<NetworkObjectReference> explode;
+        public LNetworkEvent plant;
+        public LNetworkEvent unplant;
+        public LNetworkMessage<int> plsDie;
+        /// <summary>
+        /// Puts the subspace on the floor and makes it transparent. Can be disarmed by picking it up.
+        /// </summary>
+        public void Plant(bool changeVar = true)
+        {
+            STPlugin.Log.LogInfo("Subspace Tripmine has been planted.");
+            isPlanted.Value = true;
+        }
+
+        /// <summary>
+        /// Deactivate the mine and be ready for grabbing
+        /// </summary>
+        public void Unplant(bool changeVar = true)
+        {
+            STPlugin.Log.LogInfo("Subspace Tripmine has been unplanted.");
+            isPlanted.Value = false;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            var id = this.NetworkObject.NetworkObjectId;
+            STPlugin.Log.LogInfo("Initalizing networks for Subspace #" + id);
+            explode = LNetworkMessage<NetworkObjectReference>.Connect("explode" + id);
+            isPlanted = LNetworkVariable<bool>.Connect("isPlanted" + id, default, LNetworkVariableWritePerms.Server);
+            plsDie = LNetworkMessage<int>.Connect("plsdie" + id);
+            //SubspaceTripmine.colora = LNetworkVariable<float>.Connect("colora" + id, default, LNetworkVariableWritePerms.Server);
+            plant = LNetworkEvent.Connect("plant" + id);
+            unplant = LNetworkEvent.Connect("unplant" + id);
+
+            plant.OnServerReceived += (_id) =>
+            {
+                Plant();
+            };
+            unplant.OnServerReceived += (_id) =>
+            {
+                Unplant();
+            };
+            explode.OnClientReceived += (_obj) =>
+            {
+                var obj = GetNetworkObject(_obj.NetworkObjectId);
+                //Explode(GetNetworkObject(obj.NetworkObjectId));
+                var player = obj.gameObject.GetComponent<PlayerControllerB>();
+                var light = transform.Find("ExplosionLight").gameObject.GetComponent<Light>();
+                if (light == null)
+                {
+                    STPlugin.Log.LogInfo("why.");
+                }
+                STPlugin.Log.LogDebug(player.playerUsername);
+                if (player.NetworkObject.NetworkObjectId == _obj.NetworkObjectId)
+                {
+                    if (STPlugin.instantlyKillPlayer.Value)
+                    {
+                        player.KillPlayer(Vector3.up * 100, true, CauseOfDeath.Blast);
+                    }
+                    else
+                    {
+                        player.DamagePlayer(STPlugin.damageDone.Value, true, true, CauseOfDeath.Blast);
+                    }
+                }
+                var explosionSound = STPlugin.assets.LoadAsset<AudioClip>("subspace-tripmine-explosion");
+                light.gameObject.SetActive(true);
+                light.intensity = 600;
+                if (STPlugin.enableExplosion.Value)
+                {
+                    gameObject.GetComponent<AudioSource>().clip = explosionSound;
+                    gameObject.GetComponent<AudioSource>().Play();
+                }
+                StartCoroutine("FadeOutLight", light);
+                if (STPlugin.destroyAfterExplosion.Value)
+                {
+                    transform.Find("InnerStar").gameObject.SetActive(false);
+                    transform.Find("OuterBlock").gameObject.SetActive(false);
+                    transform.Find("Trigger").gameObject.SetActive(false);
+                    Destroy(gameObject.GetComponent<BoxCollider>());
+                }
+            };
+            plsDie.OnClientReceived += (damage) =>
+            {
+                var player = StartOfRound.Instance.localPlayerController;
+                if (damage >= 100)
+                {
+                    return;
+                }
+                
+            };
+            //colora.OnValueChanged += (prev, newval) =>
+            //{
+            //    if (gameObject is null || !(gameObject is GameObject))
+            //    {
+            //        STPlugin.Log.LogDebug("gameObject is bugged, saving a NRE.");
+            //        return;
+            //    }
+            //    if (gameObject == null)
+            //    {
+            //        STPlugin.Log.LogDebug("GameObject is null :[");
+            //        return;
+            //    }
+            //    if (gameObject.GetComponentsInChildren<MeshRenderer>().Length < 1)
+            //    {
+            //        STPlugin.Log.LogDebug("0 MeshRenderers");
+            //        return;
+            //    }
+            //};
+            isPlanted.Value = false;
+
+            isPlanted.OnValueChanged += (prev, newval) =>
+            {
+                STPlugin.Log.LogDebug("isPlanted changed for subspace #" + id);
+                if (newval)
+                {
+                    fadeOut = StartCoroutine("FadeOutObject", gameObject);
+
+                }
+                else
+                {
+                    if (fadeOut is null || !(fadeOut is Coroutine)) {
+                        STPlugin.Log.LogDebug("fadeOut is bugged, saving a NRE.");
+                        return;
+                    }
+                    if (fadeOut != null) StopCoroutine(fadeOut);
+                    var mr1 = gameObject.GetComponentsInChildren<MeshRenderer>()[0];
+                    var mr2 = gameObject.GetComponentsInChildren<MeshRenderer>()[1];
+                    var newColor1 = mr1.materials[0].color;
+                    var newColor2 = mr2.materials[0].color;
+                    newColor1.a = 1;
+                    newColor2.a = 1;
+                    mr1.materials[0].color = newColor1;
+                    mr2.materials[0].color = newColor2;
+                }
+            };
+            plant.InvokeServer();
+
+
+            GetComponentInChildren<Rigidbody>().gameObject.AddComponent<GrabbableObjectPhysicsTrigger>();
+            GetComponentInChildren<GrabbableObjectPhysicsTrigger>().itemScript = this;
+
+        }
+        public override void ActivatePhysicsTrigger(Collider other)
+        {
+            //if (IsServer) return;
+            base.ActivatePhysicsTrigger(other);
+            STPlugin.Log.LogInfo(other);
+            if (other.tag == "Player" && isPlanted.Value && StartOfRound.Instance.shipHasLanded)
+            {
+                var activator = other.gameObject.GetComponentInParent<PlayerControllerB>();
+                var reference = new NetworkObjectReference(activator.NetworkObject);
+                explode.SendClients(reference);
+            }
+            
+        }
+
+        IEnumerator FadeOutLight(Light light)
+        {
+            for (float i = 0f; i < 1f; i += Time.deltaTime / 100f)
+            {
+                light.intensity = Mathf.Lerp(light.intensity, 0, i);
+                yield return null;
+            }
+            STPlugin.Log.LogInfo("Finished fading");
+        }
+
+        IEnumerator FadeOutObject(GameObject objToFade)
+        {
+            var mr1 = gameObject.GetComponentsInChildren<MeshRenderer>()[0];
+            var mr2 = gameObject.GetComponentsInChildren<MeshRenderer>()[1];
+            var newColor1 = mr1.materials[0].color;
+            var newColor2 = mr2.materials[0].color;
+            for (float i = 0f; i < 1f; i += Time.deltaTime / 75f)
+            {
+                newColor1.a = Mathf.Lerp(newColor1.a, STPlugin.plantedTransparency.Value, i);
+                newColor2.a = Mathf.Lerp(newColor2.a, STPlugin.plantedTransparency.Value, i);
+                mr1.materials[0].color = newColor1;
+                mr2.materials[0].color = newColor2;
+                yield return null;
+            }
+        }
+
+        //bool hasCalledGrabThisFrame = false;
+        //bool hasCalledReleaseThisFrame = false;
+        //bool hasHeldBefore = false;
+
+        //private void Update()
+        //{
+        //    if (!IsClient) return;
+        //    if (this.isHeld && hasCalledGrabThisFrame == false)
+        //    {
+        //        unplant.InvokeServer();
+        //        hasCalledGrabThisFrame = true;
+        //        hasCalledReleaseThisFrame = false;
+        //        hasHeldBefore = true;
+        //    }
+        //    if (!this.isHeld && hasHeldBefore && hasCalledReleaseThisFrame == false)
+        //    {
+        //        plant.InvokeServer();
+        //        hasCalledReleaseThisFrame = true;
+        //        hasCalledGrabThisFrame = false;
+        //    }
+        //}
+
+        public override void GrabItem()
+        {
+            if (!IsClient) return;
+            if (unplant == null)
+            {
+                STPlugin.Log.LogError("Plant is null. uh oh :[");
+                return;
+            }
+            unplant.InvokeServer();
+            base.GrabItem();
+
+            //hasCalledGrabThisFrame = true;
+            //hasCalledReleaseThisFrame = false;
+            //hasHeldBefore = true;
+        }
+
+        public override void DiscardItem()
+        {
+            if (!IsClient) return;
+            if (plant == null)
+            {
+                STPlugin.Log.LogError("Plant is null. uh oh :[");
+                return;
+            }
+            plant.InvokeServer();
+            base.DiscardItem();
+
+            //hasCalledReleaseThisFrame = true;
+            //hasCalledGrabThisFrame = false;
+        }
+    }
+}
